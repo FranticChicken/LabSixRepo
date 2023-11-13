988e0969a5b2a18db8c81f3529553576604aa89e
FranticChicken <emilyperron2014@gmail.com>
2023-11-12 20:27:18 -0500

    LabSix and Asset for Lab Seven

--- LabSix/Assets/Scripts/ProjectileWeapon.cs
+++ LabSix/Assets/Scripts/ProjectileWeapon.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileWeapon : MonoBehaviour
+public class ProjectileWeapon : WeaponBase
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //weaponbase instead of monobehaviour makes this script a child of the weaponBase script
+
+    [SerializeField] private Rigidbody myBullet;
+    [SerializeField] private Rigidbody myBullet2;
+    [SerializeField] private float force = 50;
+
+    protected override void Attack(float percent)
     {
-        
+        print("My weapon attacked" + percent);
+        Ray camRay = InputManager.GetCameraRay();
+        Rigidbody rb = Instantiate(percent>0.5f?myBullet2:myBullet, camRay.origin, transform.rotation);
+        rb.AddForce(Mathf.Max(percent, 0.1f) * force * camRay.direction, ForceMode.Impulse);
+        Destroy(rb.gameObject, 5);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- LabSix/Assets/Scripts/WeaponBase.cs
+++ LabSix/Assets/Scripts/WeaponBase.cs
@@ -2,17 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponBase : MonoBehaviour
+public abstract class WeaponBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Weapon Base Stats")]
+    //protected means "our children can access this field"
+    [SerializeField] protected float timeBetweenAttacks;
+    [SerializeField] protected float chargeUpTime;
+    [SerializeField, Range(0, 1)] protected float minChargePercent;
+    [SerializeField] private bool isFullyAuto;
+
+    //private variables should have underscores before them 
+    private Coroutine _currentFireTimer;
+    private bool _isOnCooldown;
+
+    private WaitForSeconds _coolDownWait;
+    private WaitUntil _coolDownEnforce;
+    private float _currentChargeTime;
+
+
+    private void Start()
     {
-        
+        _coolDownWait = new WaitForSeconds(timeBetweenAttacks);
+        _coolDownEnforce = new WaitUntil(() => !_isOnCooldown);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartShooting()
     {
-        
+        _currentFireTimer = StartCoroutine(RefireTimer());
     }
+
+    public void StopShooting()
+    {
+        StopCoroutine(_currentFireTimer);
+
+        float percent = _currentChargeTime / chargeUpTime;
+
+        //you can write if statements like this if only one thing is happening
+        if (percent != 0) TryAttack(percent);
+    }
+
+  
+
+    
+    private IEnumerator CooldownTimer()
+    {
+        _isOnCooldown = true;
+        yield return _coolDownWait;
+        _isOnCooldown = false;
+    }
+
+    //IEnumerator only works when it needs to work 
+    private IEnumerator RefireTimer()
+    {
+        print("waiting for cooldown");
+        //wait until we are not longer on cooldown
+        yield return _coolDownEnforce;
+        print("post cooldown");
+
+        while (_currentChargeTime < chargeUpTime)
+        {
+            _currentChargeTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        TryAttack(1);
+
+        yield return null;
+    }
+
+    private void TryAttack(float percent)
+    {
+        _currentChargeTime = 0;
+        if (!CanAttack(percent)) return;
+
+        Attack(percent);
+
+        StartCoroutine(CooldownTimer());
+
+        if (isFullyAuto && percent >= 1) _currentFireTimer = StartCoroutine(RefireTimer()); //Auto refire
+        
+
+    }
+
+    
+    protected virtual bool CanAttack(float percent)
+    {
+        Vector3 math = 50 * Time.deltaTime * Vector3.one;
+
+        return !_isOnCooldown && percent >= minChargePercent;
+    }
+
+    //the child will say what it this function does
+    protected abstract void Attack(float percent);
 }
